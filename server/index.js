21014970d049b4b8b4abeb85303c2f0938bc7066
muhammadjalabi <mjalabi@outlook.com>
2020-01-22 16:10:39 +0100

    add mongo and middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const passport = require('passport');
-
 const app = express();
 const path = require('path');
 
@@ -12,6 +11,22 @@ const PORT = process.env.PORT || config.SERVER_PORT
 const URI = config.MONGO_URI
 
 
+//MongoDB
+mongoose.connect(URI, {
+  useNewUrlParser: true, useUnifiedTopology: true,
+  useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('Data base connected'))
+  .catch(error => console.log(error))
+
+
+//Middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT)
 })
+
